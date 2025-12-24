fcc7c3501ed02463065ad5c0d01aeac1718a886e
Sahel <sahelmhd3@gmail.com>
2025-12-24 16:45:20 +0530

    changed hosts

--- ecomerce/settings.py
+++ ecomerce/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['52.66.249.202', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['audioaura.site', 'www.audioaura.site', '52.66.249.202', 'localhost', '127.0.0.1']
 
 # DEBUG =  'True'
 # ALLOWED_HOSTS = []
