fded26aad30609ab66d1242302f9f67674872a1a
Sahel <sahelmhd3@gmail.com>
2025-12-25 11:30:16 +0530

    refactor: remove availability button from filters

--- ecomerce/settings.py
+++ ecomerce/settings.py
@@ -30,12 +30,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+# DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['audioaura.site', 'www.audioaura.site', '52.66.249.202', 'localhost', '127.0.0.1']
+# ALLOWED_HOSTS = ['audioaura.site', 'www.audioaura.site', '52.66.249.202', 'localhost', '127.0.0.1']
 
-# DEBUG =  'True'
-# ALLOWED_HOSTS = []
+DEBUG =  'True'
+ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
